7dd2094e50bcf49869fc966e3a7b4159e91eb02b
Williams1102 <an.to@lotussoft.vn>
2022-05-04 20:39:06 +0700

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "styles/global.css";
+
+import Home from "pages";
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
